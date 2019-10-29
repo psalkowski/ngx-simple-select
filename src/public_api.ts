323d1019930910b06b40e76a58e0fea6fa269023
Piotr Sałkowski <ps@mosqi.to>
2019-10-29 10:51:42 +0100

    Remove unused service

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-simple-select
  */
 
-export * from './lib/ngx-simple-select.service';
 export * from './lib/ngx-simple-select.component';
 export * from './lib/ngx-simple-select.module';
